f6b4da7ed007bd248d456cf4896b4d031c8f497c
eMills07 <elijah.mills2@stu.daviess.kyschools.us>
2025-04-09 13:29:53 +0000

    Meh

---
+++ NumberGroup.java
@@ -0,0 +1,11 @@
+public interface NumberGroup
+{
+    int z = 1;
+    public static boolean Group1(int x,int y)
+    {
+        if(z == x || z == y)
+            return true;
+        else
+            return false;
+    }
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,16 @@
+public class Range implements NumberGroup
+{
+    int minty = 0;
+    int maxing = 0;
+    public MinMax(int Max,int Min)
+    {
+        Max = maxing;
+        Min = minty;
+    }
+
+    public _____ ___()
+    {
+        int[] array = new int[]
+    }
+}
 
